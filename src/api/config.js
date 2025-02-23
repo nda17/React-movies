998eeda489bee11d6579e30929989a785fe4bffd
Dmitry <khv1702@gmail.com>
2025-02-23 16:31:51 +0300

    Added config

---
+++ src/api/config.js
@@ -0,0 +1,3 @@
+export const API_URL = 'https://api.themoviedb.org/3';
+export const TOKEN =
+	'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYThlNzkyMTQ5YzI4MDZiZGMyN2UzMjNkZjA5MTljNiIsIm5iZiI6MTczNTU1MTc4NC42OTIsInN1YiI6IjY3NzI2YjI4YjcwMDEzY2MwZjYxNzg5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQk6IocwsC_bSPUkZK2dPxuAEbLkHKnGZuWTuMRhD9c';
