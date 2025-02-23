624b0185dc1ea79da99611f2bf9f5c99c6d7c5f3
Dmitry <khv1702@gmail.com>
2025-02-23 17:07:42 +0300

    Add config

---
+++ src/config/api.config.js
@@ -0,0 +1,4 @@
+export const API_URL = 'https://api.themoviedb.org/3';
+export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+export const TOKEN =
+	'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIxYThlNzkyMTQ5YzI4MDZiZGMyN2UzMjNkZjA5MTljNiIsIm5iZiI6MTczNTU1MTc4NC42OTIsInN1YiI6IjY3NzI2YjI4YjcwMDEzY2MwZjYxNzg5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AQk6IocwsC_bSPUkZK2dPxuAEbLkHKnGZuWTuMRhD9c';
